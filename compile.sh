a23838432592f4d60300b7194fa9781e5c9e53f0
Pedro Arvela <jp.arvela@gmail.com>
2017-09-28 19:12:36 +0100

    Fix all feedback from 1st draft

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-neato -Tpdf graphics/small-world.gv > graphics/small-world.pdf
+neato -Tpdf graphics/small_world.gv > graphics/small_world.pdf
 latexmk -pdf -pdflatex="pdflatex -interaction=nonstopmode" main.tex
