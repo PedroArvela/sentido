4e50dbdc2cf853ebf5884eab196686d8c46574a7
Pedro Arvela <jp.arvela@gmail.com>
2017-10-09 23:31:36 +0100

    Fix evaluation and conclusion

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 neato -Tpdf graphics/small_world.gv > graphics/small_world.pdf
+sfdp -Tpdf graphics/vingar_VERB_npmi_0.4_chineseWhispers.gv > graphics/vingar_VERB_npmi_0_4_chineseWhispers.pdf
 latexmk -pdf -pdflatex="pdflatex -interaction=nonstopmode" main.tex
